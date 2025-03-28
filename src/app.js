53e382088f6c7bf09e822a0d96a09f57c71d2775
martanvcs <martanvcs@gmail.com>
2025-03-28 19:48:23 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
+let who = ['El perro', 'Mi hermana', 'Mi gato', 'Mi vecino', 'Mi abuelo'];
+let action = ['robó', 'se comió', 'rompió', 'manchó', 'aplastó'];
+let what = ['mi ventana', 'mi ordenador', 'mi comida', 'mi trabajo','mi teclado'];
+let when = ['después de clase', 'cuando dormía', 'mientras comía', 'mientras leía', 'cuando trabajaba'];
 
+function excuseGenerator(who, action, what, when) {
+    let numWho = Math.floor(Math.random() * who.length);
+    let numAction = Math.floor(Math.random() * action.length);
+    let numWhat = Math.floor(Math.random() * what.length);
+    let numWhen = Math.floor(Math.random() * when.length);
+    return who[numWho] + " " + action[numAction] + " " + what[numWhat] + " " + when[numWhen];
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function onLoad() {
+    var excusa = document.getElementById('excuse');
+    excusa.innerHTML = excuseGenerator(who, action, what, when);
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = onLoad;
