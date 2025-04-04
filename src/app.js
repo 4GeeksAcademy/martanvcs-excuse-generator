3f7d236306f678745019168a5df9ad25a571fd9c
martanvcs <martanvcs@gmail.com>
2025-04-04 12:02:38 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 let who = ['El perro', 'Mi hermana', 'Mi gato', 'Mi vecino', 'Mi abuelo'];
 let action = ['robó', 'se comió', 'rompió', 'manchó', 'aplastó'];
-let what = ['mi ventana', 'mi ordenador', 'mi comida', 'mi trabajo','mi teclado'];
+let what = ['mi ventana', 'mi ordenador', 'mi comida', 'mi trabajo', 'mi teclado'];
 let when = ['después de clase', 'cuando dormía', 'mientras comía', 'mientras leía', 'cuando trabajaba'];
 
-function excuseGenerator(who, action, what, when) {
-    let numWho = Math.floor(Math.random() * who.length);
-    let numAction = Math.floor(Math.random() * action.length);
-    let numWhat = Math.floor(Math.random() * what.length);
-    let numWhen = Math.floor(Math.random() * when.length);
-    return who[numWho] + " " + action[numAction] + " " + what[numWhat] + " " + when[numWhen];
+
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
 
-function onLoad() {
-    var excusa = document.getElementById('excuse');
-    excusa.innerHTML = excuseGenerator(who, action, what, when);
+
+function excuseGenerator() {
+    return `${getRandomElement(who)} ${getRandomElement(action)} ${getRandomElement(what)} ${getRandomElement(when)}`;
 }
 
-window.onload = onLoad;
+window.onload = function () {
+    document.getElementById('excuse').innerHTML = excuseGenerator();
+};
